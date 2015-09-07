da7d41956967883466500e2f02cbb1b1a642af63
Shiwei Gong <gongshw1992@gmail.com>
2015-09-07 14:00:00 +0800

    add css reset to sidebar

--- static/js/statusBar.js
+++ static/js/statusBar.js
@@ -1,12 +1,12 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function(event) {
 
-        var html = '<div id="lighthouseMenu" class="closed">' +
+        var html = '<div class="reset-this"><div id="lighthouseMenu" class="closed">' +
             '<div class="title"><a href="https://github.com/gongshw/lighthouse">Lighthouse</a></div>' +
             '<div class="links"><a href="/">Go to lighthouse home</a>' +
             '<a href="javascript:void(0)">Visit this page directly</a></div>' +
             '<span id="menuToggle" title="点击展开Lighthouse菜单"></span>' +
-            '</div>';
+            '</div></div>';
         document.body.innerHTML += html;
         var menuToggle = document.getElementById('menuToggle');
         var lighthouseMenu = document.getElementById('lighthouseMenu');
