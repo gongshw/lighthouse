4f3713f6859454fa7d1f3be8fd6c4bfff7d60f7f
Shiwei Gong <gongshw1992@gmail.com>
2015-09-07 13:44:42 +0800

    add a sidebar button to proxied pages

--- static/js/statusBar.js
+++ static/js/statusBar.js
@@ -1,13 +1,26 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function(event) {
-        var toggleMenu = document.createElement('span')
-        toggleMenu.id = 'toggleMenu';
-        toggleMenu.title = '点击展开Lighthouse菜单'
+
+        var html = '<div id="lighthouseMenu" class="closed">' +
+            '<div class="title">Lighthouse v1.0</div>' +
+            '<div class="links"><a href="/">Go to lighthouse home</a>' +
+            '<a href="javascript:void(0)">Visit this page directly</a></div>' +
+            '<span id="menuToggle" title="点击展开Lighthouse菜单"></span>' +
+            '</div>';
+        document.body.innerHTML += html;
+        var menuToggle = document.getElementById('menuToggle');
+        var lighthouseMenu = document.getElementById('lighthouseMenu');
         var cssLink = document.createElement('link');
         cssLink.setAttribute('rel', 'stylesheet');
         cssLink.setAttribute("type", "text/css")
         cssLink.setAttribute('href', '/css/statusBar.css');
-        document.body.appendChild(toggleMenu);
         document.getElementsByTagName("head")[0].appendChild(cssLink);
+        menuToggle.addEventListener('click', function() {
+            if (lighthouseMenu.className == 'closed') {
+                lighthouseMenu.className = 'opened';
+            } else if (lighthouseMenu.className == 'opened') {
+                lighthouseMenu.className = 'closed';
+            };;
+        })
     });
 })();
